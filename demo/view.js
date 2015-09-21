6665ad9fa699ff547876841b66600a2b91430796
Frederik Krautwald <fkrautwald@gmail.com>
2015-09-21 13:57:43 +0200

    refactor(DEMO): minimize boilerplate code

--- demo/view.js
+++ demo/view.js
@@ -3,68 +3,38 @@
 import {hJSX} from '@cycle/dom'; // eslint-disable-line
 import combineClassNames from '@cyclic/util-combine-class-names';
 
+function renderSection(headline, ...vtrees) {
+  return ( // eslint-disable-line
+    <div>
+      <h4>{headline}</h4>
+      <section className={`template-DemoPages_verticalSection`}>
+        {vtrees}
+      </section>
+    </div>
+  );
+}
+
 function view({state$, id}, ...vtree$s) {
   return state$.combineLatest(
     vtree$s,
     (state, ...vtrees) => {
       const {dialogueName, className} = state;
-      const [
-        textInputLabel,
-        textInputPassword,
-        textInputIsNoFloatingLabel,
-        textInputIsDisabled,
-        textareaLabel,
-        textareaRowsMaxRows,
-        validationInputIsRequiredIsAutoValidating,
-        validationInputIsAutoValidatingPattern,
-        validationInputIsRequiredPattern,
-        charCounterInputLabel,
-        charCounterInputMaxLength,
-        charCounterTextarea,
-        prefixInputNumber,
-        suffixInput] = vtrees;
-
       return ( // eslint-disable-line
         <div
           className={combineClassNames(id, dialogueName, className,
             `template-DemoPages_sectionContainer isVertical`)}>
-          <h4>Text input</h4>
-          <section className={`template-DemoPages_verticalSection`}>
-            {textInputLabel}
-            {textInputPassword}
-            {textInputIsNoFloatingLabel}
-            {textInputIsDisabled}
-          </section>
-
-          <h4>Text area</h4>
-          <section className={`template-DemoPages_verticalSection`}>
-            {textareaLabel}
-            {textareaRowsMaxRows}
-          </section>
-
-          <h4>Validation</h4>
-          <section className={`template-DemoPages_verticalSection`}>
-            {validationInputIsRequiredIsAutoValidating}
-            {validationInputIsAutoValidatingPattern}
-            {validationInputIsRequiredPattern}
-            <br/>
-            <button className={`${dialogueName}_validateButton`}>
-              Validate!
-            </button>
-          </section>
-
-          <h4>Character counter</h4>
-          <section className={`template-DemoPages_verticalSection`}>
-            {charCounterInputLabel}
-            {charCounterInputMaxLength}
-            {charCounterTextarea}
-          </section>
-
-          <h4>Prefixes and suffixes</h4>
-          <section className={`template-DemoPages_verticalSection`}>
-            {prefixInputNumber}
-            {suffixInput}
-          </section>
+          {renderSection(`Text input`, vtrees.slice(0, 4))}
+          {renderSection(`Text area`, vtrees.slice(4, 6))}
+          {renderSection(`Validation`, vtrees.slice(6, 9).concat(
+            <div>
+              <br/>
+              <button className={`${dialogueName}_validateButton`}>
+                Validate!
+              </button>
+            </div>
+          ))}
+          {renderSection(`Character counter`, vtrees.slice(9, 12))}
+          {renderSection(`Prefixes and suffixes`, vtrees.slice(12, 14))}
         </div>
       );
     }
