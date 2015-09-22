399447e9cfabe8cc3f6b80bbcb0d8024077b60cc
Frederik Krautwald <fkrautwald@gmail.com>
2015-09-23 00:02:16 +0200

    feat(molecule-input-container): add `bindValue` and `validator` properties
    Use `bindValue` to pass the value to evaluate when your component has complex inputs.
    
    Use `validator` to pass a function that validates the current value.

--- src/molecule-input-container/model.js
+++ src/molecule-input-container/model.js
@@ -1,29 +1,39 @@
 import assign from 'fast.js/object/assign';
 
-function handleValidate(inputElement) {
-  return !inputElement.checkValidity();
-}
-
 function model({props$, actions, dialogueName}) {
   return props$.combineLatest(
     actions.focused$,
     actions.blurred$,
     actions.value$,
     actions.inputElement$,
-    (...args) => {
-      const [props, focused, blurred, value, inputElement] = args;
-      const {autoValidate, validate, type, noLabelFloat, persistLabelFloat} =
-        props;
+    (props, ...actionItems) => {
+      const [focused, blurred, value, inputElement] = actionItems;
+      const {
+        autoValidate,
+        validate,
+        type,
+        noLabelFloat,
+        persistLabelFloat,
+        bindValue,
+        validator} = props;
 
-      const isInvalid = (value !== `` || blurred) &&
-      autoValidate || validate && inputElement ?
-        handleValidate(inputElement) :
-        false;
+      let workingValue = bindValue || value;
+
+      let isInvalid = false;
+
+      const handleValidation = (workingValue !== `` || blurred) &&
+        autoValidate || validate && inputElement;
+
+      if (handleValidation && validator) {
+        isInvalid = !validator(workingValue);
+      } else if (handleValidation) {
+        isInvalid = !inputElement.checkValidity();
+      }
 
       // type="number" hack needed because value is empty until it’s valid.
       // See issue #25.
-      const inputHasContent = value || value === 0 ||
-        inputElement && type === `number` && handleValidate(inputElement);
+      const inputHasContent = workingValue || workingValue === 0 ||
+        inputElement && type === `number` && !inputElement.checkValidity();
 
       const floatLabel = !noLabelFloat && inputHasContent || persistLabelFloat;
 
