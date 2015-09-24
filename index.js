d26279667332ebd0dc9088807e33c7a4e91146d2
Frederik Krautwald <fkrautwald@gmail.com>
2015-09-24 09:52:45 +0200

    chore(): shorten import paths
    Before:
    
    ```js
    import moleculeInput from '@cyclic/molecule-input/src/molecule-input'
    ```
    
    After:
    
    ```js
    import {moleculeInput} from '@cyclic/molecule-input'
    ```
    
    Closes #26

---
+++ index.js
@@ -0,0 +1,13 @@
+import moleculeInput from './src/molecule-input';
+import moleculeInputCharCounter from './src/molecule-input-char-counter';
+import moleculeInputContainer from './src/molecule-input-container';
+import moleculeInputError from './src/molecule-input-error';
+import moleculeTextarea from './src/molecule-textarea';
+
+export {
+  moleculeInput,
+  moleculeInputCharCounter,
+  moleculeInputContainer,
+  moleculeInputError,
+  moleculeTextarea
+};
