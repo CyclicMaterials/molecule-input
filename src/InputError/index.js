067421c72ebe3ceefe58fe461156eff874d6ddf8
Frederik Krautwald <fkrautwald@gmail.com>
2015-10-03 11:35:53 +0200

    feat(InputError): add contracts for props
    References #40

--- src/InputError/index.js
+++ src/InputError/index.js
@@ -1,12 +1,20 @@
 import cuid from 'cuid';
+import predicateObjectOfObservable
+  from './../shared/predicateObjectOfObservable';
+import {Rx} from '@cycle/core';
+import propsDef from './props';
 import assign from 'fast.js/object/assign';
 import model from './../shared/model';
 import view from './view';
 
 const DIALOGUE_NAME = `molecule-InputError`;
 
-function InputError({props$}) {
+function InputError(sources) {
   const id = cuid();
+  const props$ = predicateObjectOfObservable({
+    observable$: sources.props$ || Rx.Observable.just({}),
+    defs: propsDef,
+  });
   const state$ = model({props$, dialogueName: DIALOGUE_NAME});
 
   return {
--- src/InputError/view.js
+++ src/InputError/view.js
@@ -1,11 +1,18 @@
 /** @jsx hJSX */
 
+import {string, boolean, instanceOf} from 'categories-js';
+import {Rx} from '@cycle/core';
 import {hJSX} from '@cycle/dom'; // eslint-disable-line
 import combineClassNames from '@cyclic/util-combine-class-names';
 
-function view({state$, id}) {
+function view(sources) {
+  const state$ = instanceOf(Rx.Observable)(sources.state$);
+  const id = string(sources.id);
+
   return state$.map((state) => {
-    const {dialogueName, isInvalid, errorMessage} = state;
+    const dialogueName = string(state.dialogueName);
+    const isInvalid = boolean(state.isInvalid);
+    const errorMessage = string(state.errorMessage);
 
     const classNameMod = isInvalid ? `is-invalid` : ``;
 
